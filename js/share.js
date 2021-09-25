0d0437c9ac48aed3d5208abf6b5753d007ab2243
wonjaesang <jaesangwon04@gmail.com>
2021-09-25 21:34:58 +0900

    modify share function

--- js/share.js
+++ js/share.js
@@ -1,13 +1,12 @@
 const url = 'https://ifbop.netlify.app/';
-var resultImg = document.querySelector('#resultImg1');
-var resultAlt = resultImg.firstElementChild.alt;
-const shareTitle = '내가 만약 신체부위라면 심리 테스트 결과';
-const shareDes = infoList[resultAlt].name;
-const shareImage = url + 'img/image-' + resultAlt + '.png';
-const shareURL = url + 'page/result-' + resultAlt + '.html';
 
 function setShare(){
-
+  var resultImg = document.querySelector('#resultImg1');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '내가 만약 신체부위라면 심리 테스트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
   Kakao.Link.sendDefault({
   objectType: 'feed',
   content: {
@@ -32,12 +31,24 @@ function setShare(){
 }
 
 function twitterShare() {
+  var resultImg = document.querySelector('#resultImg1');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '내가 만약 신체부위라면 심리 테스트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
   var sendText = "내가 만약 신체부위라면 심리 테스트 결과"; // 전달할 텍스트
   var sendUrl = url + 'page.result-' + resultAlt + '.html'; // 전달할 URL
   window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
 }
 
 function facebookShare() {
+  var resultImg = document.querySelector('#resultImg1');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '내가 만약 신체부위라면 심리 테스트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
   var sendUrl = url + 'page.result-' + resultAlt + '.html'; // 전달할 URL
   window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
 }
