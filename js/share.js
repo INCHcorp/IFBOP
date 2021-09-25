2d9b3aa6d722d2e68f7077efa235321f1d8a7953
wonjaesang <jaesangwon04@gmail.com>
2021-09-25 18:31:48 +0900

    modify share function

--- js/share.js
+++ js/share.js
@@ -1,45 +1,43 @@
-function kakaoShare(){
+const url = 'https://ifbop.netlify.app/';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg1');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '내가 만약 신체부위라면 심리 테스트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page.result-' + resultAlt + '.html';
+
   Kakao.Link.sendDefault({
   objectType: 'feed',
   content: {
-    title: '신체부위 테스트',
-    description: '아메리카노, 빵, 케익',
-    imageUrl:
-      'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+    title: shareTitle,
+    description: shareDes,
+    imageUrl: shareImage,
     link: {
-      mobileWebUrl: 'https://developers.kakao.com',
-      androidExecutionParams: 'test',
-    },
-  },
-  social: {
-    likeCount: 10,
-    commentCount: 20,
-    sharedCount: 30,
-  },
-  buttons: [
-    {
-      title: '웹으로 이동',
-      link: {
-        mobileWebUrl: 'https://developers.kakao.com',
+      mobileWebUrl: shareURL,
+      WebUrl: shareURL
       },
     },
+    buttons: [
     {
-      title: '앱으로 이동',
+      title: '결과확인하기',
       link: {
-        mobileWebUrl: 'https://developers.kakao.com',
+        mobileWebUrl: shareURL,
+        WebUrl: shareURL
+        },
       },
-    },
-  ]
-});
+    ]
+  });
 }
 
 function twitterShare() {
-  var sendText = "나를 대표하는 신체부위"; // 전달할 텍스트
-  var sendUrl = "devpad.tistory.com/"; // 전달할 URL
+  var sendText = "내가 만약 신체부위라면 심리 테스트 결과"; // 전달할 텍스트
+  var sendUrl = url + 'page.result-' + resultAlt + '.html'; // 전달할 URL
   window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
 }
 
 function facebookShare() {
-  var sendUrl = "https://ifbop.netlify.app/"; // 전달할 URL
+  var sendUrl = url + 'page.result-' + resultAlt + '.html'; // 전달할 URL
   window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
 }
