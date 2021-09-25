1e4c8a076eb3669ebcb1191f6966b7d6f591fca2
wonjaesang <jaesangwon04@gmail.com>
2021-09-25 21:42:06 +0900

    modify share function

--- js/share.js
+++ js/share.js
@@ -38,7 +38,7 @@ function twitterShare() {
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
   var sendText = "내가 만약 신체부위라면 심리 테스트 결과"; // 전달할 텍스트
-  var sendUrl = url + 'page.result-' + resultAlt + '.html'; // 전달할 URL
+  var sendUrl = url + 'page/result-' + resultAlt + '.html'; // 전달할 URL
   window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
 }
 
@@ -49,6 +49,6 @@ function facebookShare() {
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
-  var sendUrl = url + 'page.result-' + resultAlt + '.html'; // 전달할 URL
+  var sendUrl = url + 'page/result-' + resultAlt + '.html'; // 전달할 URL
   window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
 }
